bd964ea8e3104c5ab61ef8de0d3402449f46c0b8
joaofelipe <joaofelipe1294@gmail.com>
2023-09-02 17:20:35 -0300

    feat: finish Flask API.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify, request
 from keras.models import load_model
 
+# Carrega o modelo de classificação antes de iniciar a API.
 model = load_model('./model.h5')
-
-print(model.summary())
 
 # Define o objeto que faz referência a aplicação.
 app = Flask(__name__)
@@ -12,14 +11,19 @@ app = Flask(__name__)
 @app.route('/predict', methods=['POST'])
 def predict():
   payload = request.get_json()
+  # Recupera o atributo com as features
   features = payload['features']
+
+  # Classifica o exemplar
   result = model.predict([features])
 
-  # attributes = payload['']
+  # É preciso converter o resultado do modelo em uma lista, pois o tipo do dado retornado pelo método
+  # keras.Sequential.predict é um np.array e este objeto não pode ser serializado para JSON através
+  # do método jsonify
+  result_list = result.tolist()
 
-  # return jsonify(prediction_result=result[0])
-
-  return jsonify(predictions=result)
+  # Retorna uma lista com a probabilidade de cada uma das classes.
+  return jsonify(predictions=result_list[0])
 
 # Inicia a aplicação.
-app.run(debug=True)
+app.run()
