fd2d9649c6684f1f2913d11bfac67625c60aceba
joaofelipe <joaofelipe1294@gmail.com>
2023-09-02 19:31:41 -0300

    feat: add if condition

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def predict():
   return jsonify(predictions=result_list[0])
 
 # Inicia a aplicação.
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
-app.run()
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
+  app.run()
